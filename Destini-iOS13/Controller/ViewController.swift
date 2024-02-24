7e49131a824d4e8cf2611cd5098fc834e6852efc
Maksim Stogniy <mstogniy@yandex.com>
2024-02-25 00:14:10 +0300

    Complete app

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
+//  Updated by Maksim Stogniy on 24.02.2024.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -14,10 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.currentTitle!
+        
+        storyBrain.nextStory(userChoice: choice)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let currentStory = storyBrain.getCurrentStory()
+        storyLabel.text = currentStory.title
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
+    }
+    
 
 
 }
